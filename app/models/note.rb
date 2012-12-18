dd8018839bcc6e9d4d72b99ca334b847e2cc1790
gloriaf <mgfernandez@pabilos.com>
2012-12-18 15:40:28 +0100

    corrige ordern en tabla Notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,6 +2,8 @@ class Note < ActiveRecord::Base
   attr_accessible :english, :spanish, :sequence, :text
   belongs_to :topic
   
+  default_scope order: 'sequence, created_at DESC'
+  
   validates :topic_id, presence: true
   validates :sequence, presence: true, :uniqueness => { :scope => :topic_id, :message => "Sequence must be unique to the topic" }, numericality: { greater_than: 0 }
   validates :text,     presence: true
